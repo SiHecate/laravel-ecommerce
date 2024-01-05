02d6479d48ab509e8bee444be8ca4678ba054fd1
SiHecate <blrumutcan64@gmail.com>
2024-01-05 08:33:21 +0300

    Routing pratice

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('deneme', function() {
+        return ("hello this is testing for auth system is working.");
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('deneme', function() {
+        return ("hello this is testing for auth system is working.");
+    });
+});
