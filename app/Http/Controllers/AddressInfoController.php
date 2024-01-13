15283264d9d9d749ad874cc4da7e1881037d6a2a
SiHecate <blrumutcan64@gmail.com>
2024-01-13 23:34:29 +0300

    controller begin

--- app/Http/Controllers/AddressInfoController.php
+++ app/Http/Controllers/AddressInfoController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AddressInfo;
 use Illuminate\Http\Request;
 
 class AddressInfoController extends Controller
 {
-    //
+    protected function validationRules()
+    {
+        return [
+            'address_name' => ['required'],
+            'user_id' => ['required', 'numeric'],
+            'name' => ['required'],
+            'lastname' => ['required'],
+            'telephone' => ['required'],
+            'city' => ['required'],
+            'county' => ['required'],
+            'neighborhood' => ['required'],
+            'full_address' => ['required'],
+        ];
+    }
+
+    public function index()
+    {
+        $addressInfo = AddressInfo::all();
+        return response()->json(['addressInfo' => $addressInfo], 200);
+    }
+
+    public function store(Request $request)
+    {
+
+    }
 }
