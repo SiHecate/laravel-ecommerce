4e832182f8553552e39b707f27138beb41f762ee
SiHecate <blrumutcan64@gmail.com>
2024-02-16 04:01:37 +0300

    Auth middleware

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,12 @@ use App\Http\Controllers\BasketController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserInfoController;
 
+require __DIR__.'/auth.php';
+
+
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
-
-require __DIR__.'/auth.php';
 
 Route::get('token', function() {
     $token = csrf_token();
@@ -18,7 +19,6 @@ Route::get('token', function() {
     ]);
 });
 
-
 Route::get('/dashboard', function() {
     return "dashboard page";
 });
@@ -26,12 +26,12 @@ Route::get('/dashboard', function() {
 // Routing içerisinde token döndürülmemesi için accept : application-json
 
 /*
-    Routing problemlerini çözdüm gibi eğer ki bu mesaj hala daha buradaysa çözememişimdir....
+    Routing problemlerini çözdüm gibi eğer ki bu mesaj hala daha buradaysa çözememişimdir....   
 
 */
 
 // Ürün Rotaları
-Route::prefix('product')->group(function () {
+Route::prefix('product')->middleware('auth')->group(function () {
     Route::get('/', [ProductController::class, 'index'])->name('product.index');
     Route::post('/search', [ProductController::class, 'search'])->name('product.search');
     Route::post('/add', [ProductController::class, 'store'])->name('product.store');
@@ -41,7 +41,7 @@ Route::prefix('product')->group(function () {
 });
 
 // Sepet Rotaları
-Route::prefix('basket')->group(function () {
+Route::prefix('basket')->middleware('auth')->group(function () {
     Route::get('/', [BasketController::class, 'index'])->name('basket.index');
     Route::post('/', [BasketController::class, 'store'])->name('basket.store');
     Route::put('/{id}/{type}', [BasketController::class, 'update'])->name('basket.update');
@@ -51,7 +51,7 @@ Route::prefix('basket')->group(function () {
 });
 
 // Kullanıcı Bilgisi Rotaları
-Route::prefix('address')->group(function () {
+Route::prefix('address')->middleware('auth')->group(function () {
     Route::get('/', [UserInfoController::class, 'index'])->name('address.index');
     Route::post('/', [UserInfoController::class, 'store'])->name('address.store');
     Route::put('/', [UserInfoController::class, 'update'])->name('address.update');
