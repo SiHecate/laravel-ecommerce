61d2f0c6726914cd4e823ef87bc34145f7cdb238
SiHecate <blrumutcan64@gmail.com>
2024-02-20 16:45:51 +0300

    Route update

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Middleware\AdminCheck;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BasketController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserInfoController;
+use App\Http\Middleware\AdminCheck;
 
 require __DIR__.'/auth.php';
 
@@ -13,27 +13,6 @@ require __DIR__.'/auth.php';
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
-
-Route::get('token', function() {
-    $token = csrf_token();
-    return response()->json([
-        'csrf_token' => $token
-    ]);
-});
-
-Route::get('/dashboard', function() {
-    return "dashboard page";
-});
-
-Route::get('/user/profile', function () {
-    $user = Auth::user();
-    return $user;
-})->middleware('auth');
-
-Route::get('user/adminCheck', function() {
-    return 'Horray';
-})->middleware(AdminCheck::class);
-
 
 // Routing içerisinde token döndürülmemesi için accept : application-json
 
@@ -66,27 +45,32 @@ Route::get('user/adminCheck', function() {
         - User ? 
 */ 
 
-// Ürün Rotaları
+// Product routes
 Route::prefix('product')->group(function () {
+
     Route::get('/', [ProductController::class, 'index'])->name('product.index');
     Route::post('/search', [ProductController::class, 'search'])->name('product.search');
-    Route::post('/add', [ProductController::class, 'store'])->middleware('auth')->name('product.store');
     Route::get('/{id}', [ProductController::class, 'show'])->name('product.show');
-    Route::put('/{id}', [ProductController::class, 'update'])->middleware('auth')->name('product.update');
-    Route::delete('/{id}', [ProductController::class, 'destroy'])->middleware('auth')->name('product.destroy');
+
+    // Admin routes
+    Route::middleware(['auth', AdminCheck::class])->group(function () {
+        Route::post('/add', [ProductController::class, 'store'])->name('product.store');
+        Route::put('/{id}', [ProductController::class, 'update'])->name('product.update');
+        Route::delete('/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+    });
 });
 
-// Sepet Rotaları
-Route::prefix('basket')->group(function () {
+// Basket routes
+Route::prefix('basket')->middleware('auth')->group(function () {
     Route::get('/', [BasketController::class, 'index'])->name('basket.index');
-    Route::post('/', [BasketController::class, 'store'])->middleware('auth')->name('basket.store');
-    Route::put('/{id}/{type}', [BasketController::class, 'update'])->middleware('auth')->name('basket.update');
-    Route::delete('/{id}', [BasketController::class, 'destroy'])->middleware('auth')->name('basket.destroy');
-    Route::get('/sepet', [BasketController::class, 'view'])->middleware('auth')->name('basket.view');
-    Route::get("/paymentBasket", [BasketController::class, 'basket'])->middleware('auth')->name('basket.payment');
+    Route::post('/', [BasketController::class, 'store'])->name('basket.store');
+    Route::put('/{id}/{type}', [BasketController::class, 'update'])->name('basket.update');
+    Route::delete('/{id}', [BasketController::class, 'destroy'])->name('basket.destroy');
+    Route::get('/sepet', [BasketController::class, 'view'])->name('basket.view');
+    Route::get("/paymentBasket", [BasketController::class, 'basket'])->name('basket.payment');
 });
 
-// Kullanıcı Bilgisi Rotaları
+// Address routes
 Route::prefix('address')->middleware('auth')->group(function () {
     Route::get('/', [UserInfoController::class, 'index'])->name('address.index');
     Route::post('/', [UserInfoController::class, 'store'])->name('address.store');
@@ -94,3 +78,24 @@ Route::prefix('address')->middleware('auth')->group(function () {
     Route::get('/info', [UserInfoController::class, 'show'])->name('address.show');
     Route::delete('/{id}', [UserInfoController::class, 'delete'])->name('address.delete');
 });
+
+// Diğer rotalar
+Route::get('token', function() {
+    $token = csrf_token();
+    return response()->json([
+        'csrf_token' => $token
+    ]);
+});
+
+Route::get('/dashboard', function() {
+    return "dashboard page";
+});
+
+Route::get('/user/profile', function () {
+    $user = Auth::user();
+    return $user;
+})->middleware('auth');
+
+Route::get('user/adminCheck', function() {
+    return 'Horray';
+})->middleware(AdminCheck::class);
