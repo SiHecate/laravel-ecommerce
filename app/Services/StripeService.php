efec5a9f66cfa49ce8972ede300106c2dd500193
SiHecate <blrumutcan64@gmail.com>
2024-02-26 12:15:31 +0300

    Beggining of the day

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -4,8 +4,19 @@ namespace App\Services;
 
 class StripeService{
     
+    public function index() {
+        // Product return but we have already product controller 
+    }
 
     public function checkout() {
+
+        // Stripe secret key
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        // Product
+
+        
+
 
     }
 }
